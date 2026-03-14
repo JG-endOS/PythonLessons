bd83960f9c4a0cb5e3fe801c5bbe2719dd3d7964
João Gabriel <jgfnascimento15@gmail.com>
2026-03-14 15:56:08 -0300

    Update Ex002_IMC.py

--- Ex002_IMC.py
+++ Ex002_IMC.py
@@ -5,4 +5,4 @@ peso = 57
 
 IMC = peso/altura**2
 
-print(f'{nome} tem {idade} anos, mede {altura}m e pesa {peso}Kg. Seu IMC é {IMC}')
+print(f'{nome} tem {idade} anos, mede {altura}m e pesa {peso}Kg. Seu IMC é {IMC:.2f}')
