5f4e7b327f7a0a2ef5afd2f5937c8cfbc4d16eaf
João Gabriel <jgfnascimento15@gmail.com>
2026-03-14 15:34:27 -0300

    Add files via upload

---
+++ Ex001.py
@@ -0,0 +1,13 @@
+nome = 'João'
+sobrenome = 'Gabriel'
+idade = 19
+ano_nacimento = 2007
+maior_de_idade = True
+altura_em_metros = 1.64
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de nascimento:', ano_nacimento)
+print('Maior de idade:', maior_de_idade)
+print('Altura:', altura_em_metros)
